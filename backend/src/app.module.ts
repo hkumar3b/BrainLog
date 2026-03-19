ec0d97e4bda4b2a1b62a1f94857e69f644e02ef8
Himanshu kumar <himanshukumarjnv12345@gmail.com>
2026-03-19 12:38:31 +0530

    feat: update NestJS backend project with Prisma and PostgreSQL integration.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { LinksModule } from './links/links.module';
 
 @Module({
-  imports: [PrismaModule, LinksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule],
 })
-export class AppModule {}
-
+export class AppModule { }
